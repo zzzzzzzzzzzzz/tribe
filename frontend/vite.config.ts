344dae97a05f9bfa75d770ef479e30595f3e6ee2
Dmitry Denisenko <dmitriy.denisenko@outlook.com>
2025-01-25 22:21:45 +0800

    Update vite.config.ts
    Signed-off-by: Dmitry Denisenko <dmitriy.denisenko@outlook.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
   plugins: [react(), TanStackRouterVite()],
 })
