94c96c7a6242a12a086a465145470dcdf48881f5
ZZZZZZZZZZZZZ <dmitriy.denisenko@outlook.com>
2025-02-12 22:09:39 +0800

    Removed anthropic model from choices

--- frontend/src/components/Members/EditMember.tsx
+++ frontend/src/components/Members/EditMember.tsx
@@ -73,12 +73,6 @@ const customSelectOption = {
 // TODO: Place this somewhere else.
 const AVAILABLE_MODELS = {
   openai: ["gpt-4o-mini", "gpt-4o", "gpt-4-turbo", "gpt-3.5-turbo"],
-  anthropic: [
-    "claude-3-haiku-20240307",
-    "claude-3-sonnet-20240229",
-    "claude-3-opus-20240229",
-  ],
-  ollama: ["llama3.1"],
   gigachat: ["GigaChat", "GigaChat-Plus", "GigaChat-Pro", "GigaChat-Max"],
 }
 
